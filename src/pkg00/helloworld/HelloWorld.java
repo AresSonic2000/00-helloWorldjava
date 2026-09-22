7cc6690c9965e3ee45d4241ce1cef7fa9a3386d8
abreilletkerforn <abreilletkerforn@350028R-F115X05.lise.kreizenn.bzh>
2026-09-22 17:30:07 +0200

    Exercice 4

--- src/pkg00/helloworld/HelloWorld.java
+++ src/pkg00/helloworld/HelloWorld.java
@@ -10,8 +10,9 @@
  * @since 22/09/2026
  * @see pkg00.HelloWorld#test(float)
  */
-package pkg00.helloworld;
 
+package pkg00.helloworld;
+import java.util.Scanner;
 /**
  * Classe principale contenant la méthode main.
  * 
@@ -24,10 +25,26 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        /*System.out.println("Hello World Arès, cv ? ");*/
+        //Hello World
+        //System.out.println("Hello World Arès, cv ? ");*/
+        //Exercice 2
+        /*
         System.out.println("Message : Ligne 1");
         System.out.println("Message : Ligne 2");
-        
+        */
+        int x, y, tmp;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entre la valeur de x :");
+        x = sc.nextInt();
+        System.out.println("Entre la valeur de y :");
+        y = sc.nextInt();
+        System.out.println("Valeur de x avant l'echange :" + x);
+        System.out.println("Valeur de y avant l'echange :" + y);
+        tmp = x;
+        x = y;
+        y = tmp;
+        System.out.println("Valeur de x apres l'echange :" + x);
+        System.out.println("Valeur de y apres l'echange :" + y);
     }
     
 }
