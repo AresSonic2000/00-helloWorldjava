39a9d5a33186d5a21e93065e2f52bb81feb59159
Arès Sonic 2000 <aaron.breillet@gmail.com>
2026-09-22 16:13:31 +0200

    print message
    Le message de la fonction pour écrire dans l'invite de commande à été changer

--- src/pkg00/helloworld/HelloWorld.java
+++ src/pkg00/helloworld/HelloWorld.java
@@ -24,7 +24,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World Arès !");
+        System.out.println("Hello World Arès, cv ? ");
     }
     
 }
