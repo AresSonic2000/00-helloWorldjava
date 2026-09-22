f5b30e3d375e88e60c8b91e8d74e1a76790e1c4d
abreilletkerforn <abreilletkerforn@350028R-F115X05.lise.kreizenn.bzh>
2026-09-22 15:36:35 +0200

    ler commit : ajout fichier java

---
+++ src/pkg00/helloworld/HelloWorld.java
@@ -0,0 +1,30 @@
+/**
+ * Classe contenant le point d'entrée du programme pour des exemples simples.
+ * 
+ * Cette Classe illustre un Hello minimal ainsi qu'une méthode
+ * de conversion simple.
+ * 
+ * @file HelloWorld.java
+ * @author ABK
+ * @version 0.0
+ * @since 22/09/2026
+ * @see pkg00.HelloWorld#test(float)
+ */
+package pkg00.helloworld;
+
+/**
+ * Classe principale contenant la méthode main.
+ * 
+ * @author abreilletkerforn
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }
+    
+}
